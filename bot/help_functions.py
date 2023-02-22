a7f5f293e29424bcbccfc2407a3f96436da619ba
AndrejKovacik57 <andrejkovacik57@gmail.com>
2023-02-22 22:03:06 +0100

    fixed get cb day

--- bot/help_functions.py
+++ bot/help_functions.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timezone
 import re
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent  # pip install fake-useragent
